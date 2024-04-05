a7777b5a4ac0c9addbf3102dddf9fa2bbb18b651
Ruben Arcos <ruben.arcos06@gmail.com>
2024-04-05 12:26:50 -0500

    fixed the about me text

--- components/about.tsx
+++ components/about.tsx
@@ -28,8 +28,8 @@ export default function About() {
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">React, Next.js, Node.js, and MySql</span>.
-        I am also familiar with TypeScript. I am always looking to learn new
-        technologies. I am currently looking for a{" "}
+        I am also familiar with TypeScript, and always looking to learn new
+        technologies. I'm currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
