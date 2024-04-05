f17319188a24828508f584a5b733ebfc6072d95c
Ruben Arcos <ruben.arcos06@gmail.com>
2024-04-05 12:33:31 -0500

    changed the text in the about me section again

--- components/about.tsx
+++ components/about.tsx
@@ -19,8 +19,8 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-      After graduating with a degree in{" "}
-        <span className="font-medium">Nursing</span>, I decided to pursue my
+      After being a {" "}
+        <span className="font-medium">nurse</span> for over six years, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
