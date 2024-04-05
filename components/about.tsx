f9f58a2eda1b91f0f4ea4b424046b065fe53722b
Ruben Arcos <ruben.arcos06@gmail.com>
2024-04-05 12:17:51 -0500

    changed text in the about me section.

--- components/about.tsx
+++ components/about.tsx
@@ -36,11 +36,10 @@ export default function About() {
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        video games, traveling, and playing with my pets. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="font-medium">gardening</span>, so I can grow my own fruits and veggies.
       </p>
     </motion.section>
   );
