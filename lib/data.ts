1dcab1f207c0032d41e2ae6ed06507b0f6062d82
Ruben Arcos <ruben.arcos06@gmail.com>
2024-04-05 02:08:05 -0500

    modified files

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
+import myCertsImg from "@/public/myCerts.png"
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
@@ -35,32 +36,39 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Software Engineer",
+    location: "Austin, TX",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I graduated after 5 months of studying. Collaborated with a team of developers to build modern and responsive web applications using best practices",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "Nov 2023",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Software Engineer",
+    location: "Los Angeles, CA",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      " Designed and executed full-stack web projects with semantic structure I'm now working as a freelancer. My stack includes React, Next.js, TypeScript, and Tailwind. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2023 - present",
+  },
+  {
+    title: "ER Nurse",
+    location: "Lancaster, TX",
+    description:
+      "Managed a full range of care from traumas and cardiac arrests to the administration of medications for up to 20 patients per shift. Documented data related to patients with 100% accuracy and confidentiality.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2022 - Present",
   },
 ] as const;
 
 export const projectsData = [
+  {
+    title: "myCerts",
+    description:
+      "Application to save important documents such as certificates/licenses. This app will keep all your important documents safe and readily available anywhere",
+    tags: ["React", "AWS", "MySql Workbench", "Tailwind"],
+    imageUrl: myCertsImg,
+  },
   {
     title: "CorpComment",
     description:
@@ -94,14 +102,14 @@ export const skillsData = [
   "Node.js",
   "Git",
   "Tailwind",
-  "Prisma",
-  "MongoDB",
+  // "Prisma",
+  // "MongoDB",
   "Redux",
-  "GraphQL",
-  "Apollo",
+  // "GraphQL",
+  // "Apollo",
   "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
+  // "PostgreSQL",
+  // "Python",
+  // "Django",
   "Framer Motion",
 ] as const;
